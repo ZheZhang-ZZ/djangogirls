494bad7edb40931bfb5ddee44baebf8f60d66a76
Zhe Zhang <zzsjtu1988@163.com>
2020-05-23 22:56:05 +0800

    first task

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/zhezhang/Desktop/icloud/0-tongbu/2-learning/python/djangogirls/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
